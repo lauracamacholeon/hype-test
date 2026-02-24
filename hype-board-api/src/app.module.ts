780858956fe2588d53d6b645e41d121c8b12717e
Laura Camacho <lauracamacholeon@gmail.com>
2026-02-24 16:33:11 -0500

    add helmet and throttler

--- hype-board-api/src/app.module.ts
+++ hype-board-api/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { VideosModule } from './videos/videos.module';
 
 @Module({
-  imports: [VideosModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    VideosModule,
+  ],
 })
 export class AppModule {}
